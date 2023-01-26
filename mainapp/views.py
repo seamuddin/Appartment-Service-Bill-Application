dc6d70f39aac00d7017d0823efe645774ed297fd
seamuddin <seamuddin2020@gmail.com>
2023-01-26 10:28:48 +0600

    >> Log In Page File Structure Prepare

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request,**kwargs):
-    return render(request,'index.html')
+    return render(request,'dashboard/index.html')
+
+
+def login(request,**kwargs):
+    return render(request,'login/login.html')
--- servicebill/urls.py
+++ servicebill/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('servicebill.urls'))
+    path('', include('mainapp.urls')),
 ]
