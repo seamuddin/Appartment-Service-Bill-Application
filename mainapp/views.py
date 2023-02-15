6b4da6c718bffb8e9900069c5417f6947ab0c5bc
seamuddin <seamuddin2020@gmail.com>
2023-02-15 10:11:45 +0600

    >> Bill Add Page

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,7 +6,9 @@ from django.views.generic import View
 from servicebill import settings
 # Create your views here.
 from tanent.models import Tanent
-
+from .models import BillHistory
+from flat.models import Flat
+import calendar
 
 @login_required(login_url='login')
 def index(request,**kwargs):
@@ -28,10 +30,34 @@ class LogoutView(View):
 
 @login_required(login_url='login')
 def generate_bill(request,**kwargs):
+    if request.POST:
+        bill_history = BillHistory()
+        bill_history.month = request.POST.get('month')
+        bill_history.year = request.POST.get('year')
+        bill_history.amount = request.POST.get('amount') if request.POST.get('amount') else 400
+        bill_history.status = 0
+        bill_history.tanent = Tanent.objects.get(id = request.POST.get('tanent'))
+        bill_history.clean()
+        bill_history.save()
+
+
+        bill_info = {}
+        bill_info['tanent'] = bill_history.tanent.name
+        bill_info['month'] = calendar.month_name[int(bill_history.month)]
+        bill_info['year'] = bill_history.year
+        bill_info['amount'] = bill_history.amount
+        bill_info['status'] = bill_history.status
+        bill_info['flat_no'] = bill_history.tanent.flat.flat_no
+        bill_info['size'] = bill_history.tanent.flat.size
+        bill_info['amount'] = bill_history.amount
+        return render(request,'bill/bill_info.html',context=bill_info)
+
+
     context = {
         'tanents': Tanent.objects.all(),
         'years': year_list(2020,2100),
-        'month': month_list()
+        'month': month_list(),
+        'flats': Flat.objects.all()
     }
     return render(request,'bill/add.html', context=context)
 
@@ -48,6 +74,7 @@ def month_list():
     import calendar
     month = []
     for i in range(1,13):
+
         month.append({'name' : calendar.month_name[i], 'number' : i})
 
 
